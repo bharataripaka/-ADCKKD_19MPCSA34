c6e9c8922b00d61d89e7fa3e4153837b8458282a
bharataripaka <63902806+bharataripaka@users.noreply.github.com>
2021-06-30 21:00:42 +0530

    Add files via upload

---
+++ h.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class h
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int x=s.nextInt();
+int y=s.nextInt();
+String z=String.valueOf(n);
+String z1=String.valueOf(x);
+String z2=String.valueOf(y);
+System.out.println(z.replace(z1,z2));
+}}
